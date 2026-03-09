06c07ec690abe689a9fe5ce57a81fa468c973c36
Sean Boots <sean.boots@gmail.com>
2026-03-09 10:56:39 -0700

    Add net new packages and their associated resources from GeoYukon

--- add_net_new_datasets.R
+++ add_net_new_datasets.R
@@ -50,18 +50,71 @@ slugify <- function(x) {
     iconv(from = 'UTF-8', to = 'ASCII//TRANSLIT') %>% # Convert accented characters to ASCII before removing non-ASCII below
     str_replace_all("[^a-z0-9\\s-]", "") %>%  # Remove non-alphanumeric (except spaces/hyphens)
     str_squish() %>%                          # Remove extra whitespace
-    str_replace_all("\\s+", "-")              # Replace spaces with hyphens
+    str_replace_all("\\s+", "-") %>%          # Replace spaces with hyphens
+    str_replace_all("-{2,}", "-")             # Replace multiple hyphens with singular ones
 }
 
 get_name_from_title <- function(title) {
   slugify(title)
 }
 
-add_geospatial_layer_ckan_package <- function(title, description, homepage_url, tags) {
+get_date_stamp <- function() {
+  
+  date_stamp <- as.character(now())
+  
+  date_stamp |> 
+    str_sub(0L, 10L) |> 
+    str_replace_all("-", "")
+    
+  
+}
+
+convert_dcat_keywords_to_tags <- function(keywords, filter_short_tags = TRUE, add_geoyukon_datestamp = TRUE) {
+  
+  # tags <- tibble(name = net_new_datasets$keyword[[1]])
+  tags <- tibble(raw_name = keywords)
+  
+  tags <- tags |> 
+    mutate(
+      name = get_name_from_title(raw_name)
+    ) |> 
+    select(name) |> 
+    distinct()
+  
+  if(filter_short_tags == TRUE) {
+    
+    tags <- tags |> 
+      filter(
+        str_length(name) > 2
+      )
+    
+  }
+  
+  if(add_geoyukon_datestamp == TRUE) {
+    
+    new_row = tibble_row(
+      name = str_c("geoyukon-import-", get_date_stamp())
+    )
+    
+    tags <- tags |>
+      bind_rows(
+        new_row
+      ) |> 
+      arrange(name)
+    
+  }
+  
+  tags
+  
+}
+
+add_geospatial_layer_ckan_package <- function(title, description, homepage_url, keywords = list()) {
   
   cat("Adding ", title, "to CKAN.\n")
   
-  package_create(
+  tags <- convert_dcat_keywords_to_tags(keywords)
+  
+  newly_created_package <- package_create(
     name = get_name_from_title(title),
     title = title,
     notes = description,
@@ -72,19 +125,51 @@ add_geospatial_layer_ckan_package <- function(title, description, homepage_url, 
     extras = list(
       internal_contact_email = "Geomatics.Help@yukon.ca",
       internal_contact_name = "Geomatics Yukon",
+      homepage_url = homepage_url
     ),
     
+    # REVIEW: Includes the first entry but not subsequent ones??
+    # groups = data.frame(
+    #   # Nature and environment
+    #   id = "46be34d1-443f-4188-9639-692f2bda0e14",
+    #   # Science and technology
+    #   id = "a8dc4319-ae14-4568-b49e-57334fd3fa31"
+    # ),
+    # 
     groups = data.frame(
-      # Nature and environment
-      id = "46be34d1-443f-4188-9639-692f2bda0e14",
-      # Science and technology
-      id = "a8dc4319-ae14-4568-b49e-57334fd3fa31"
-    )
+      id = c(
+        # Nature and environment
+        "46be34d1-443f-4188-9639-692f2bda0e14",
+        # Science and technology
+        "a8dc4319-ae14-4568-b49e-57334fd3fa31"
+      )
+    ),
+    
+    tags = tags
+    
+    
     
   )
   
-  # TODO: add keywords/tags
-  # TODO: add resource links to the newly-created resource
+  resources_to_add <- dcat_resources |> 
+    filter(dataset_title == title)
+  
+  for (i in seq_along(resources_to_add$resource_url)) { 
+    
+    cat("Adding resource ", resources_to_add$resource_title[i], "\n")
+    
+    resource_create(
+      package_id = newly_created_package$id,
+      rcurl = resources_to_add$resource_url[i],
+      name = resources_to_add$resource_title[i],
+      format = "HTML"
+    )
+    
+    Sys.sleep(0.1)
+    
+  }
+  
+  Sys.sleep(0.3)
   
 }
 
@@ -94,3 +179,19 @@ net_new_datasets <- net_new_datasets_dcat |>
   )
 
 net_new_datasets$description
+
+
+for (i in seq_along(net_new_datasets$title)) { 
+  
+  add_geospatial_layer_ckan_package(
+    net_new_datasets$title[i],
+    net_new_datasets$description[i],
+    net_new_datasets$arcgis_url[i],
+    # Note syntax for list contents for keyword here
+    net_new_datasets$keyword[[i]]
+  )
+  
+  # For testing, just run one item:
+  # break;
+  
+}
